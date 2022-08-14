62fc58b76a182bd2a6af4366ea766a0827f577b8
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:24:44 +0100

    integrate the content template should be string test

--- src/js/notify/notifyRender.test.js
+++ src/js/notify/notifyRender.test.js
@@ -8,4 +8,8 @@ describe('test render notify by titleTemplate and contentTemplate', () => {
     it('should renderNotify([], "password is required") throw error with "the returned title is not string"', () => {
         expect(() => renderNotify([], "password is required")).toThrowError('the returned title is not string')
     });
+
+    it('should renderNotify("validation error", {}) throw error with "the returned content is not string"', () => {
+        expect(() => renderNotify("validation error", {})).toThrowError('the returned content is not string')
+    });
 })
