9ce25821838ad1a3cf47e6262016cace3969b932
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:16:49 +0100

    integrate the notifyRender must be defined test

---
+++ src/js/notify/notifyRender.test.js
@@ -0,0 +1,5 @@
+describe('test render notify by titleTemplate and contentTemplate', () => {
+    it('should renderNotify("", "") defined', () => {
+        expect(renderNotify("", "")).toBeDefined()
+    });
+})
