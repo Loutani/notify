6c16ec229736b62d71409fcb58a94660570d9779
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:00:56 +0100

    should return content text test

--- src/js/notify/notifyContent.test.js
+++ src/js/notify/notifyContent.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import {createContentTemplate} from './notifyContent'
 
 describe('test create notify content template', () => {
@@ -7,5 +11,17 @@ describe('test create notify content template', () => {
 
     it('createContentTemplate([]) should throw error exception with "content must be string"', () => {
         expect(() => createContentTemplate([])).toThrowError('content must be string');
+    });
+
+    it('createContentTemplate("email is required") should return "validation error" as string', () => {
+        let contentTemplate = createContentTemplate('email is required'),
+        contentTemplateAsElement = document.createElement('div');
+
+        contentTemplateAsElement.innerHTML = contentTemplate;
+        document.body.appendChild(contentTemplateAsElement);
+
+        let content = document.querySelector('.toast-body').textContent;
+
+        expect(content).toBe('email is required')
     });
 });
