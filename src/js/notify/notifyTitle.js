84c9ae4f52fc314c3544e879ee52e851e4c23499
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 17:19:30 +0100

    resolve the title not string type test

--- src/js/notify/notifyTitle.js
+++ src/js/notify/notifyTitle.js
@@ -1,3 +1,7 @@
 export const createContentTemplate = (title) => {
+    if(typeof title !== 'string') {
+        throw new Error('title must be string')
+    }
+
     return true
 }
--- src/js/notify/notifyTitle.test.js
+++ src/js/notify/notifyTitle.test.js
@@ -2,7 +2,7 @@ import {createContentTemplate} from './notifyTitle';
 
 describe('test create notify title template', () => {
     it('createContentTemplate() should be defined', () => {
-        expect(createContentTemplate()).toBeDefined();
+        expect(createContentTemplate('')).toBeDefined();
     });
 
     it('createContentTemplate([]) should throw error exception with "title must be string"', () => {
