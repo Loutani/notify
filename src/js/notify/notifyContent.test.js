f63c5a38eb8d72813e16646168b1151c31569143
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:05:28 +0100

    integrate content should return element test and resolve it

--- src/js/notify/notifyContent.test.js
+++ src/js/notify/notifyContent.test.js
@@ -24,4 +24,16 @@ describe('test create notify content template', () => {
 
         expect(content).toBe('email is required')
     });
+
+    it('createContentTemplate("<li>email is required</li>") should contain <li> element', () => {
+        let contentTemplate = createContentTemplate('<li>email is required</li>'),
+        contentTemplateAsElement = document.createElement('div');
+
+        contentTemplateAsElement.innerHTML = contentTemplate;
+        document.body.appendChild(contentTemplateAsElement);
+
+        let content = document.querySelector('.toast-body li').textContent;
+
+        expect(content).toBe('email is required')
+    });
 });
