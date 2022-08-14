ddd93e2b1197f55390299411efbac752d1bfa883
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:19:22 +0100

    resolve the renderNotify should be defined test

---
+++ src/js/notify/notifyRender.js
@@ -0,0 +1,3 @@
+export const renderNotify = (titleTemplate, contentTemplate) => {
+    return true;
+}
--- src/js/notify/notifyRender.test.js
+++ src/js/notify/notifyRender.test.js
@@ -1,3 +1,5 @@
+import {renderNotify} from './notifyRender'
+
 describe('test render notify by titleTemplate and contentTemplate', () => {
     it('should renderNotify("", "") defined', () => {
         expect(renderNotify("", "")).toBeDefined()
