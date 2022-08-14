0ca06f7b2d756c8cb8a57807f09c249fe80d8072
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 18:22:17 +0100

    integrate should throw error when the title template is not string

--- src/js/notify/notifyRender.test.js
+++ src/js/notify/notifyRender.test.js
@@ -4,4 +4,8 @@ describe('test render notify by titleTemplate and contentTemplate', () => {
     it('should renderNotify("", "") defined', () => {
         expect(renderNotify("", "")).toBeDefined()
     });
+
+    IDBTransaction('should renderNotify([], "password is required") throw error with "the returned title is not string"', () => {
+        expect(() => renderNotify([], "password is required")).toThrowError('the returned title is not string')
+    });
 })
