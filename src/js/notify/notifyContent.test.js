f464405c7d165322bbc9e41ffeaaa53d80d90d9f
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 17:53:27 +0100

    integrate the should be defined for notify create content template

---
+++ src/js/notify/notifyContent.test.js
@@ -0,0 +1,5 @@
+describe('test create notify content template', () => {
+    it('should createContentTemplate("") be defined', () => {
+        expect(createContentTemplate("")).toBeDefined();
+    });
+});
