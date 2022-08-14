5df37287e8a2f6d5ef08db19b5f12e315b3e11dd
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 17:56:27 +0100

    integrate throwing error whe content is not string

--- src/js/notify/notifyContent.test.js
+++ src/js/notify/notifyContent.test.js
@@ -4,4 +4,8 @@ describe('test create notify content template', () => {
     it('should createContentTemplate("") be defined', () => {
         expect(createContentTemplate("")).toBeDefined();
     });
+
+    it('createContentTemplate([]) should throw error exception with "content must be string"', () => {
+        expect(() => createContentTemplate("")).toThrowError('content must be string');
+    });
 });
