18b57535fef6029d2b13a431fd1661a56395b54a
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-08-14 17:11:16 +0100

    title template must be defined test

---
+++ src/js/notify/notifyTitle.test.js
@@ -0,0 +1,5 @@
+describe('test create notify title template', () => {
+    it('createContentTemplate() should be defined', () => {
+        expect(createContentTemplate()).toBeDefined();
+    });
+});
